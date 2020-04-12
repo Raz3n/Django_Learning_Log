8d8cb3117c40da6950f5e3b3bc910c38d6f4d4a4
Nelson <nbritob@hotmail.com>
2020-04-12 15:39:44 +0100

    adds new topic view

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Topic
+from .forms import TopicForm
 
 def index(request):
     """Homepage for Learning Log"""
@@ -17,3 +18,19 @@ def topic(request, topic_id):
     entries = topic.entry_set.order_by('-date_added')
     context = {'topic': topic, 'entries': entries}
     return render(request, 'learning_logs/topic.html', context)
+
+def new_topic(request):
+    """adds a new topic"""
+    if request.method != 'POST':
+        # No data submitted; creates a blank form
+        form = TopicForm()
+    else:
+        # Post data submitted; process data
+        form = TopicForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('learning_logs:topics')
+        
+    # Display a blank or invalid form
+    context = {'form': form}
+    return render(request, 'learning_logs/new_topic.html', context)
