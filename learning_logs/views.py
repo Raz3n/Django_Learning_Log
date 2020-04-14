dd8aa46dbfaac557188af301886631014a438905
Nelson <nbritob@hotmail.com>
2020-04-14 16:07:58 +0100

    edit entry works

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -58,9 +58,8 @@ def edit_entry(request, entry_id):
     """Edit an existing entry."""
     entry = Entry.objects.get(id=entry_id)
     topic = entry.topic
-    
-    if request.method != 'Post':
-        # Inital request; pre-fill form with current entry.
+    if request.method != 'POST':
+        # Initial request; pre-fill form with the current entry.
         form = EntryForm(instance=entry)
     else:
         # POST data submitted; process data.
